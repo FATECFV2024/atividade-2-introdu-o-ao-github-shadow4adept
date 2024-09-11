5fecae1024708ccbd59082fc728f47957144311f
Hirota <93461467+shadow4adept@users.noreply.github.com>
2024-09-11 11:46:25 -0300

    [feat] RGM Class

---
+++ FazerVsCode/src/_2920482411007.java
@@ -0,0 +1,11 @@
+public class _2920482411007 {
+    public static void main(String[] args) throws Exception {
+        String name = "Gustavo Hirota Alves Velho";
+        long rgm = 2920482411007L;
+        int age = 19;
+        String usergithub = "@shadow4adept";
+        String userlinkedin = "@shadow4adept";
+        String courseclass = "Análise e Desenvolvimento de Sistemas / 2º Semestre";
+        System.out.println("Nome: "+name+"\nRGM: "+rgm+"\nIdade: "+age+"\nUsuário do GitHub: "+usergithub+"\nUsuário do LinkedIn: "+userlinkedin+"\nCurso e Turma: "+courseclass);
+    }
+}
