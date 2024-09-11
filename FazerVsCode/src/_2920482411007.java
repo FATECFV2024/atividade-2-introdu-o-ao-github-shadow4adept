e3b06480405a307ca09c6459fab908094e904461
Hirota <93461467+shadow4adept@users.noreply.github.com>
2024-09-11 11:52:39 -0300

    [refactor] Renomeando LinkedIn
    Neste commit foi realizado a alteração da variável userlinkedin, além da implementação de comentários.

--- FazerVsCode/src/_2920482411007.java
+++ FazerVsCode/src/_2920482411007.java
@@ -1,11 +1,13 @@
-public class _2920482411007 {
+public class _2920482411007 { // Classe com RGM
     public static void main(String[] args) throws Exception {
+        // Criação das variáveis
         String name = "Gustavo Hirota Alves Velho";
         long rgm = 2920482411007L;
         int age = 19;
         String usergithub = "@shadow4adept";
-        String userlinkedin = "@shadow4adept";
+        String userlinkedin = "@gustavo-hirota-alves-velho-28b45323b";
         String courseclass = "Análise e Desenvolvimento de Sistemas / 2º Semestre";
+        // Output das variáveis com informações pessoais
         System.out.println("Nome: "+name+"\nRGM: "+rgm+"\nIdade: "+age+"\nUsuário do GitHub: "+usergithub+"\nUsuário do LinkedIn: "+userlinkedin+"\nCurso e Turma: "+courseclass);
     }
 }
